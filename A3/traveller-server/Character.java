a25e53ce7f2784647c82ed4ebd704da6c0228722
anuk18 <kandasamy.a@husky.neu.edu>
2021-01-30 23:49:44 -0500

    Create Character.java

---
+++ A3/traveller-server/Character.java
@@ -0,0 +1,21 @@
+package World;
+import World.Town;
+
+public class Character {
+    private String name;
+    private Town town;
+
+    public Character(String n, Town t) {
+        this.name = n;
+        this.town = t;
+    }
+
+    public Town getTown() {
+        return this.town;
+    }
+
+    public void setTown(Town t) {
+        this.town = t;
+    }
+
+}
