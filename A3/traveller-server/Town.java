4f73218416917dbdb6a250c13c0213be5425cc03
anuk18 <kandasamy.a@husky.neu.edu>
2021-01-30 23:49:22 -0500

    Create Town.java

---
+++ A3/traveller-server/Town.java
@@ -0,0 +1,42 @@
+ package World;
+import World.Character;
+
+import java.util.Set;
+
+public class Town {
+    private String name;
+    private Set<Town> neighbors;
+    private Set<Character> residents;
+
+    public Town(String n,Set<Town> ne, Set<Character> r ) {
+        this.name = n;
+        this.neighbors = ne;
+        this.residents = r;
+    }
+
+    public Set<Town> getNeighbors() {
+        return this.neighbors;
+    }
+
+    //added this method to help with getOpenRoute
+    public String getName() {
+        return this.name;
+    }
+
+    public void addResident(Character c) {
+        this.residents.add(c);
+    }
+
+    public Set<Character> getResidents() {
+        return this.residents;
+    }
+
+    public boolean hasResidents() {
+        return !(this.residents.isEmpty());
+    }
+
+
+
+
+
+}
